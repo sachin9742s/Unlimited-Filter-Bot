69d5000d15b4ad2e99e97d581af8c57ee794f65e
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-01 17:17:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("KICCHA REQUEST", url="https://t.me/KicchaRequest"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Movie Channel", url="https://t.me/GD_FILMCLUB"),
+                    InlineKeyboardButton("Support CHANNEL", url="https://t.me/ROCKHDMOVIES2021")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "KICCHA REQUEST", url="https://t.me/KicchaRequest")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
